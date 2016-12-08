f3e8e065a738dd635f9a6751c71197d5a0474ffc
D.Aoki <vienosnotes@gmail.com>
2016-12-08 15:24:59 +0900

    Cosmetic fix

---
+++ Swiftspear/Converters/PlayButtonColorConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Swiftspear.Converters
+{
+    class PlayButtonColorConverter : IValueConverter
+    {
+        private readonly static Style PlayingStyle;
+        private readonly static Style StoppedStyle;
+
+        static PlayButtonColorConverter()
+        {
+            PlayingStyle = new Style(typeof(Label));
+            PlayingStyle.Setters.Add(new Setter(Label.BackgroundProperty, Brushes.Transparent));
+            PlayingStyle.Setters.Add(new Setter(Label.ForegroundProperty, Brushes.LimeGreen));
+
+            StoppedStyle = new Style(typeof(Label));
+            StoppedStyle.Setters.Add(new Setter(Label.BackgroundProperty, Brushes.Transparent));
+            StoppedStyle.Setters.Add(new Setter(Label.ForegroundProperty, Brushes.Gray));
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.GetType() != typeof(bool) || targetType != typeof(Style))
+            {
+                throw new ArgumentException();
+            }
+
+            // is playing
+            if ((bool)value)
+            {
+                return PlayingStyle;
+            }
+            else
+            {
+                return StoppedStyle;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Swiftspear/ViewModels/AnalyzerWindowViewModel.cs
+++ Swiftspear/ViewModels/AnalyzerWindowViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Livet;
 using Livet.Commands;
 using Microsoft.Win32;
+using OxyPlot;
+using Swiftspear.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,6 +15,8 @@ namespace Swiftspear.ViewModels
 {
     class AnalyzerWindowViewModel : ViewModel
     {
+        private readonly AnalyzeModel _model = new AnalyzeModel();
+
         private readonly string _fileName;
         
         public string Title
@@ -23,9 +27,29 @@ namespace Swiftspear.ViewModels
             }
         }
 
+        public bool IsPlaying
+        {
+            get
+            {
+                return _model.IsPlaying;
+            }
+        }
+
+        public IEnumerable<DataPoint> TimeDomain
+        {
+            get { return null; } // _model.LatestReceived?.Select((p, idx) => new DataPoint(idx, p)); }
+        }
+
+        public IEnumerable<DataPoint> FrequencyDomain
+        {
+            get { return null; } // _model.FreqSpectrum.Select(t => new DataPoint(t.Item1 * 10, t.Item2 / 1000)); }
+        }
+
         public AnalyzerWindowViewModel(string fileName)
         {
             _fileName = fileName;               
         }
+
+
     }
 }
